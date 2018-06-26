ec742e99e687ecb6f58ae60e7c5f4e49f67e9eca
Abdulqadir <a.q.r@hotmail.co.uk>
2018-06-26 21:49:13 +0100

    add player walk & idle animation, add player movement

---
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed = 5f;
+
+	private Rigidbody2D myBody;
+	private Animator anim;
+
+	void Awake(){
+		// gets component 'Rigidbody2D' from the current object
+		myBody = GetComponent<Rigidbody2D>(); 
+		// gets component 'Animator' from the current object
+		anim = GetComponent<Animator>();
+
+	}
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+
+	// called in fixed intervals, defined by TimeManager -> Fixed Timestep
+	void FixedUpdate(){
+		PlayerWalk();
+	}
+
+	void PlayerWalk(){
+		// GetAxisRaw - returns integers based on input; -1 for left, 1 for right
+		float h = Input.GetAxisRaw("Horizontal");
+		if(h>0){
+			// move 'myBody' right, keep y-axis static
+			myBody.velocity = new Vector2(speed, myBody.velocity.y);
+		}else if(h<0){
+			myBody.velocity = new Vector2(-speed, myBody.velocity.y);
+		}
+	}
+}
