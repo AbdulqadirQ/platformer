c5fb630cc2e4c474cd3cfc1b41be0b7e85b02985
Abdulqadir <a.q.r@hotmail.co.uk>
2018-07-07 13:25:04 +0100

    add frog parent to persist animation transform. add FrogIdleRight, FrogJumpRight

--- Assets/Scripts/Enemy Scripts/FrogScript.cs
+++ Assets/Scripts/Enemy Scripts/FrogScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FrogScript : MonoBehaviour {
-
+	// FIXME: FrogJumpLeft animation jumps over x-axis further than FrogJumpRight 
 	private Animator anim;
 
 	private bool animation_Started;
@@ -22,17 +22,33 @@ public class FrogScript : MonoBehaviour {
 		StartCoroutine(frog_jump_coroutine);
 	}
 	
-	void Update () {
-		
+	// is called at the end of each frame, i.e. after the Update()
+	void LateUpdate () {
+		// if animation has finished AND started
+		if(animation_Finished && animation_Started){
+			animation_Started = false; // only set so this if-statement is executed again
+
+			// sets parent game object (Frog Parent) to equal the current transform
+			transform.parent.position = transform.position;
+
+			// sets the 'Frog' game object (child) to the current vector (i.e. after 
+			// the animation has been played, therefore recording the end animation's position)
+			transform.localPosition = Vector3.zero;
+		}
 	}
 
 	IEnumerator FrogJump(){
 		yield return new WaitForSeconds(Random.Range(1f, 4f));
 
+		animation_Started = true;
+		animation_Finished = false;
+
+		jumpedTimes++;
+
 		if(jumpLeft){
 			anim.Play("FrogJumpLeft");
 		}else{
-
+			anim.Play("FrogJumpRight");
 		}
 
 		StartCoroutine(frog_jump_coroutine);
@@ -43,6 +59,24 @@ public class FrogScript : MonoBehaviour {
 	// function at a particular frame. It's used here so at the last frame of 'FrogJumpLeft',
 	// the frog is set to an idle animation, rather than repeating 'FrogJumpLeft'
 	void AnimationFinished(){
-		anim.Play("FrogIdleLeft");
+		animation_Finished = true;
+
+		if(jumpLeft){
+			anim.Play("FrogIdleLeft");
+		}else{
+			anim.Play("FrogIdleRight");
+		}
+
+		// frog will jump left 3 times, before jumping right 3 times, and so on
+		if(jumpedTimes == 3){
+			jumpedTimes = 0;
+
+			Vector3 tempScale = transform.localScale;
+			// switch to opposite scale (direction)
+			tempScale.x *= -1;
+			transform.localScale = tempScale;
+
+			jumpLeft = !jumpLeft;
+		}
 	}
 }
