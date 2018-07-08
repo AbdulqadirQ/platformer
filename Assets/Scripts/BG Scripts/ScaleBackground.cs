023d53c6e714fc355e8d2f1d96eb5d9dec8bb3e8
Abdulqadir <a.q.r@hotmail.co.uk>
2018-07-08 13:34:16 +0100

    create level design. Resize background

---
+++ Assets/Scripts/BG Scripts/ScaleBackground.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleBackground : MonoBehaviour {
+
+	void Start () {
+		SpriteRenderer sr = GetComponent<SpriteRenderer>();
+
+		transform.localScale = new Vector3(1,1,1);
+
+		// width and height of sprite
+		float width = sr.sprite.bounds.size.x;
+		float height = sr.sprite.bounds.size.y;
+
+		// calculates world height and width of the camera
+		float worldScreenHeight = Camera.main.orthographicSize * 2f;
+		float worldScreenWidth = worldScreenHeight / Screen.height * Screen.width;
+
+		Vector3 tempScale = transform.localScale;
+		tempScale.x = worldScreenWidth / width + 0.1f;
+		tempScale.y = worldScreenHeight / height + 0.1f;
+
+		transform.localScale = tempScale;
+
+	}
+	
+	void Update () {
+		
+	}
+}
